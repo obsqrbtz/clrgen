7f00732bb76f5f9edba37fe0d8898c8ffc80bfb5
Daniel Dada <doesdeos@gmail.com>
2023-12-12 20:50:20 +0300

    made proper template system

--- src/gen.rs
+++ src/gen.rs
@@ -1,76 +1,16 @@
 use crate::common;
 
-pub fn generate_kitty_colors(colors: &Vec<String>) -> Vec<String> {
+pub fn generate_hex(colors: &Vec<String>, template_name: String) -> Vec<String> {
     let mut config: Vec<String> = vec![String::new(); 44];
     let mut count = 0;
     if let Ok(lines) 
-        = common::read_lines(format!("{}/.clrgen/templates/kitty.conf", common::get_home_dir())) {
+        = common::read_lines(format!("{}/.clrgen/templates/{}", common::get_home_dir(), template_name)) {
         for line in lines {
-            if let Ok(conf_line) = line {
-                if !conf_line.starts_with("#") && conf_line.len() != 0{
-                    let conf_line_split : Vec<_> = conf_line.split_whitespace().map(str::to_string).collect();
-                    let option = &conf_line_split[0];
-                    let num_str = &conf_line_split[1].replace("$", "");
-                    let num_int = num_str.parse::<u8>()
-                        .expect("Can not parse 'kitty.conf' template");
-                    let value = &colors[num_int as usize].to_string();
-                    config[count] = format!("{} {}", option, value);
-                }else{
-                    config[count] = conf_line;
+            if let Ok(mut conf_line) = line {
+                for i in (0..18).rev(){
+                    conf_line = conf_line.replace(&format!("${}", i),&colors[i]);
                 }
-                count +=1;
-            }
-        }
-    }
-    config
-}
-
-pub fn generate_alacritty_colors(colors: &Vec<String>) -> Vec<String> {
-    let mut config: Vec<String> = vec![String::new(); 44];
-    let mut count = 0;
-    if let Ok(lines) 
-        = common::read_lines(format!("{}/.clrgen/templates/alacritty.yml", common::get_home_dir())) {
-        for line in lines {
-            if let Ok(conf_line) = line {
-                if !conf_line.starts_with("#") && !conf_line.ends_with(":") && conf_line.len() != 0{
-                    let conf_line_split : Vec<_> = conf_line.split_whitespace().map(str::to_string).collect();
-                    let option = &conf_line_split[0];
-                    let num_str = &conf_line_split[1].replace("$", "");
-                    let num_int = num_str.parse::<u8>()
-                        .expect("Can not parse 'alacritty.conf' template");
-                    let value = &colors[num_int as usize].to_string().replace("#", "");
-                    config[count] = format!("    {} '0x{}'", option, value);
-                }else{
-                    config[count] = conf_line;
-                }
-                count +=1;
-            }
-        }
-    }
-    config
-}
-
-pub fn generate_i3_colors(colors: &Vec<String>) -> Vec<String> {
-    let mut config: Vec<String> = vec![String::new(); 44];
-    let mut count = 0;
-    if let Ok(lines) 
-        = common::read_lines(format!("{}/.clrgen/templates/i3config", common::get_home_dir())) {
-        for line in lines {
-            if let Ok(conf_line) = line {
-                if !conf_line.starts_with("#") && !conf_line.contains(['{', '}']) && conf_line.len() != 0{
-                    let conf_line_split : Vec<_> = conf_line.split_whitespace().map(str::to_string).collect();
-                    let option = &conf_line_split[0];
-                    config[count] = format!("{:<23} ", option);
-                    for i in 1..conf_line_split.len(){
-                        let num_str = &conf_line_split[i].replace("$", "");
-                        let num_int = num_str.parse::<u8>()
-                            .expect("Can not parse 'i3config' template");
-                        let value = &colors[num_int as usize].to_string();
-                        config[count] = format!("{}{:<12}", config[count], value);
-                    }
-                }else{
-                    config[count] = conf_line;
-                }
+                config[count] = conf_line;
                 count +=1;
             }
         }
--- src/main.rs
+++ src/main.rs
@@ -14,17 +14,23 @@ fn main() {
                     let num_int = num_str.parse::<u8>()
                     .expect(&format!("Can not parse color number at line {} in '{}'", count, file_name));
                     let color = &conf_line_split[1];
-                    colors[num_int as usize] = color.to_string();
+                    colors[num_int as usize] = color.to_string().replace("#", "");
                 }
                 count +=1;
             }
         }
     }
-    let conf_kitty = gen::generate_kitty_colors(&colors);
-    let conf_alacritty = gen::generate_alacritty_colors(&colors);
-    let conf_i3 = gen::generate_i3_colors(&colors);
+    let conf_kitty = gen::generate_hex(&colors, "kitty.conf".to_string());
+    let conf_i3 = gen::generate_hex(&colors, "i3config".to_string());
+    let conf_alacritty = gen::generate_hex(&colors, "alacritty.yml".to_string());
+    let conf_eww = gen::generate_hex(&colors, "eww.scss".to_string());
+    let conf_sway = gen::generate_hex(&colors, "sway".to_string());
+    let conf_waybar = gen::generate_hex(&colors, "waybar.css".to_string());
     common::write_conf(format!("{}/.clrgen/kitty_clrgen.conf", common::get_home_dir()), conf_kitty);
     common::write_conf(format!("{}/.clrgen/alacritty_clrgen.yml", common::get_home_dir()), conf_alacritty);
     common::write_conf(format!("{}/.clrgen/i3_clrgen", common::get_home_dir()), conf_i3);
+    common::write_conf(format!("{}/.clrgen/eww.scss", common::get_home_dir()), conf_eww);
+    common::write_conf(format!("{}/.clrgen/sway", common::get_home_dir()), conf_sway);
+    common::write_conf(format!("{}/.clrgen/waybar.css", common::get_home_dir()), conf_waybar);
 
 }
