065fd0933acc2e63ec0e2794e9d79b4662d2f8fa
Daniel Dada <doesdeos@gmail.com>
2023-12-13 12:29:49 +0000

    fixed install.sh

--- install.sh
+++ install.sh
@@ -4,4 +4,5 @@ mkdir -p ~/.config/clrgen/colors
 mkdir -p ~/.clrgen
 cp -rf templates/* ~/.config/clrgen/templates/
 cp -rf colors/* ~/.config/clrgen/colors/
+cp -f clrgen.toml ~/.config/clrgen/
 sudo cp clrgen /usr/local/bin/
